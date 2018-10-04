2028a68bd3f2657cbff4089e0778fb9392753162
Matsunaga <matsunaga.kenji@canon-elec.co.jp>
2018-10-04 21:17:30 +0900

    LedDriverのテスト環境を構築

---
+++ chap3/test/LedDriverTest.c
@@ -0,0 +1,16 @@
+#include "unity_fixture.h"
+
+TEST_GROUP(LedDriver);
+
+TEST_SETUP(LedDriver)
+{
+}
+
+TEST_TEAR_DOWN(LedDriver)
+{
+}
+
+TEST(LedDriver, LedsOffAfterCreate)
+{
+    // TEST_FAIL_MESSAGE("Start here");
+}
---
+++ chap3/test/LedDriverTestRunner.c
@@ -0,0 +1,6 @@
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(LedDriver)
+{
+    RUN_TEST_CASE(LedDriver, LedsOffAfterCreate);
+}
